fb7fe4d68ae0862c5069b80a414f37cf2c564ed2
Aghyad-Fanous <aghyad.fanous.work@gmail.com>
2025-10-31 13:30:44 +0300

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -96,7 +96,7 @@ export function Hero() {
                 style={{ width: '420px', height: '420px' }}
               >
                 <img
-                  src="https://images.unsplash.com/photo-1650255612136-c4f048a49624?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjeWJlcnB1bmslMjBuZW9uJTIwcG9ydHJhaXR8ZW58MXx8fHwxNzYxNzIwNTYxfDA&ixlib=rb-4.1.0&q=80&w=1080"
+                  src="https://drive.google.com/file/d/1z_f9NiVYSLFAs3LZqOPuADUhIvWdk9NT/view?usp=drivesdk"
                   alt={t('hero.profileAlt')} 
                   className="w-full h-full object-cover"
                 />
